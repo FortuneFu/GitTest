e5f7596fe79e3a9ab496f2b0dca22c4f1cc434b6
Fortune <lachesis.f@gmail.com>
2015-01-07 17:20:17 +0800

    first 	new file:   .classpath 	new file:   .project 	new file:   README 	new file:   pom.xml 	modified:   src/main/java/net/f/GitTest/App.java 	new file:   target/.DS_Store 	new file:   target/classes/.DS_Store 	new file:   target/classes/net/.DS_Store 	new file:   target/classes/net/f/.DS_Store 	new file:   target/classes/net/f/GitTest/.DS_Store 	new file:   target/classes/net/f/GitTest/App.class 	new file:   target/test-classes/net/f/GitTest/AppTest.class

--- src/main/java/net/f/GitTest/App.java
+++ src/main/java/net/f/GitTest/App.java
@@ -4,10 +4,7 @@ package net.f.GitTest;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+	}
 }
