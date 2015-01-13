d61d58aed0bf89c75d35dae883fb3b25ce1c4bef
Fortune <lachesis.f@gmail.com>
2015-01-13 14:42:11 +0800

    made a testing commit[C

--- src/main/java/net/f/GitTest/App.java
+++ src/main/java/net/f/GitTest/App.java
@@ -6,6 +6,6 @@ package net.f.GitTest;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello world 20150113!");
+		System.out.println("Hello world 20150113 testing!");
 	}
 }
